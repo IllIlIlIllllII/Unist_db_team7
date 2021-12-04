e086a727d5845b6dbfa2c19337998b59677f8ba0
uni20181068 <wlsgur4011@unist.ac.kr>
2021-12-04 15:29:31 +0900

    .

---
+++ mysql_test.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(user='root', password='root',
+                              host='ec2-3-38-108-28.ap-northeast-2.compute.amazonaws.com',
+                              database='bbgg')
+
+cursor = cnx.cursor()
+cursor.execute("SELECT * FROM Product")
+
+result = cursor.fetchall()
+
+for x in result:
+  print(x)
+cnx.close()
